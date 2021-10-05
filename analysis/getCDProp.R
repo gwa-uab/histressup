43ead3d73b686013f03f5c1fc923b81d01b09922
gwa_ualberta <gwa_ualberta@penguin>
2021-10-05 09:56:50 -0600

    stuff

---
+++ analysis/getCDProp.R
@@ -0,0 +1,25 @@
+
+#setwd("/mnt/chromeos/GoogleDrive/SharedDrives/FES_Data_Sharing_Forestry/Manuscripts/histressupp/analysis")
+setwd("g:/Shared drives/FES_Data_Sharing_Forestry/Manuscripts/histressupp/analysis")
+
+
+library(tidyverse)
+
+CDProp <-
+  read.csv(file = '../data/species_proportions_full.csv', fileEncoding = 'UTF-8-BOM')
+
+CDProp <- CDProp %>%
+  mutate(dProp = popu_tre) %>%
+  mutate(cProp = abie_bal + abie_las + pice_eng + pice_gla + pice_mar + 
+           pinu_ban + pinu_con + pseu_men) %>%
+  mutate(tProp = dProp + cProp) %>%
+  mutate(cProp = cProp / tProp) %>%
+  mutate(dProp = dProp / tProp) %>%
+  filter(tProp > 0.99) %>%
+  select(TRM, cProp, dProp)
+
+CDProp
+
+save(CDProp,file="../data/CDProp.RData")
+
+head(CDProp)
